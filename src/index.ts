72a35f5a6fc581b9306e8aa8fcbcf61437efa379
25LP3402 25LP3402 <25LP3402@G303-CC0130.local>
2025-06-23 18:51:57 +0800

    XO

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,12 @@ const connectDb = async () => {
 };
 app.post("/addUser", async (req: Request, res: Response) => {
   try {
-    const response = db
-      .collection("testUsers")
-      .insertOne({ name: "Yagami", age: "34000", email: "ahah@gmail" });
-    res.json((await response).insertedId.getTimestamp());
-// res.json(response)
+    const response = db.collection("testUsers").insertMany([
+      { name: "Ustgal", age: "19" },
+      { name: "Ustagl2", age: "90" },
+    ]);
+    res.json((await response).insertedIds);
+    // res.json(response)
     // res.send("OK");
   } catch (error) {
     console.log(error);
@@ -41,25 +42,42 @@ app.post("/addUser", async (req: Request, res: Response) => {
 });
 
 app.get("/", async (req: Request, res: Response) => {
-  // const client = new MongoClient(uri);
+  // const client = new MongoClient(url);
   // await client.connect();
 
   //YMAR DB-S AWAHAA BICHIJ UGNU.
   // const db = client.db("sample_mflix");
 
   //TUHAIN DB-D BAIGAA COLLECTION-G BICHEJ UGNU
-
   const responses = db.collection("testUsers").find(); //find - aa todorhoilj uguugui bol bugdiin awch irne!
 
   //RESPONSE ARRAY BOLGOPJ HUWIRGAN.
   const users = await responses.toArray();
+
   console.log("Users", users);
 
   res.json(users);
-  // res.send({
-  //   HELLO_GUYS:
-  //     "Zalhuuraad bailgui udriin hiih zuilsiinhee jagsaaltaa garga🤌🏻🧠",
-  // });
+});
+
+app.delete("/del", async (req: Request, res: Response) => {
+  const response = db.collection("testUsers").deleteMany({ name: "Johan" });
+  console.log("DELTED RESPONSE", response);
+  res.send("Bolormaa");
+});
+
+app.put("/upDate", async (req: Request, res: Response) => {
+  const responses = db
+    .collection("testUsers")
+    .updateMany({ name: "Yagami" }, { $set: { age: "18" } });
+  console.log("UPDATED RESPONSES", await responses);
+
+  res.json(responses);
+});
+
+app.get("/selectUser", async (req: Request, res: Response) => {
+  const responses = db.collection("testUsers").find({ name: "Johan" });
+  const selelcted = await responses.toArray();
+  res.json(selelcted);
 });
 
 app.listen(port, async () => {
